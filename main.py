e8f20527dd611c57a47775a034ea2e1e0e98202a
Levi Eduardo Villarreal <levi.eduardo2024@gmail.com>
2025-07-19 08:57:17 -0600

    n500

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ CORS(app, resources={r"/*": {"origins": [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "https://web-production-21f2a.up.railway.app",
-    "https://TU_DOMINIO_NETLIFY",
-    "https://tusitio.netlify.app",
-    "https://soporteches.online"
+    "https://soporteches.online",
+    "https://sistemadesplegableboo-production.up.railway.app"
 ]}}, supports_credentials=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventario.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
